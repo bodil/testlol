85865bee58dcde55c2c0646e95c13796dbfc53b2
Bodil Stokke <bodil@bodil.tv>
2010-12-14 16:42:10 +0100

    Make readFile/readUrl methods (as in Rhino Shell) available in the test scope.

--- src/main/java/tv/bodil/testlol/Shell.java
+++ src/main/java/tv/bodil/testlol/Shell.java
@@ -7,6 +7,7 @@
 package tv.bodil.testlol;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,12 +16,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.EvaluatorException;
 import org.mozilla.javascript.Function;
 import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.ScriptRuntime;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.WrappedException;
@@ -39,7 +42,7 @@ public class Shell extends ScriptableObject {
         super();
         this.testlol = testlol;
         cx.initStandardObjects(this);
-        String[] names = { "print", "load", "getClasspathResource", "getLolTimer" };
+        String[] names = { "print", "load", "getClasspathResource", "getLolTimer", "readFile", "readUrl" };
         defineFunctionProperties(names, Shell.class, ScriptableObject.DONTENUM);
         Scriptable props = cx.newObject(this);
         props.put("testSuite", props, Context.javaToJS(testlol.getTestSuite(),
@@ -155,4 +158,139 @@ public class Shell extends ScriptableObject {
         return Calendar.getInstance().getTimeInMillis();
     }
 
+    // Nicked a few methods from the Rhino shell source to clone readFile/readUrl functions
+    
+    public static Object readFile(Context cx, Scriptable thisObj,
+            Object[] args, Function funObj) throws IOException {
+        if (args.length == 0) {
+            throw Context.reportRuntimeError("readFile() takes at least one argument.");
+        }
+        String path = ScriptRuntime.toString(args[0]);
+        String charCoding = null;
+        if (args.length >= 2) {
+            charCoding = ScriptRuntime.toString(args[1]);
+        }
+
+        return readUrl(path, charCoding, true);
+    }
+
+    public static Object readUrl(Context cx, Scriptable thisObj, Object[] args,
+            Function funObj) throws IOException {
+        if (args.length == 0) {
+            throw Context.reportRuntimeError("readUrl() takes at least one argument.");
+        }
+        String url = ScriptRuntime.toString(args[0]);
+        String charCoding = null;
+        if (args.length >= 2) {
+            charCoding = ScriptRuntime.toString(args[1]);
+        }
+
+        return readUrl(url, charCoding, false);
+    }
+
+    private static String readUrl(String filePath, String charCoding,
+            boolean urlIsFile) throws IOException {
+        int chunkLength;
+        InputStream is = null;
+        try {
+            if (!urlIsFile) {
+                URL urlObj = new URL(filePath);
+                URLConnection uc = urlObj.openConnection();
+                is = uc.getInputStream();
+                chunkLength = uc.getContentLength();
+                if (chunkLength <= 0)
+                    chunkLength = 1024;
+                if (charCoding == null) {
+                    String type = uc.getContentType();
+                    if (type != null) {
+                        charCoding = getCharCodingFromType(type);
+                    }
+                }
+            } else {
+                File f = new File(filePath);
+                if (!f.exists()) {
+                    throw new FileNotFoundException("File not found: "
+                            + filePath);
+                } else if (!f.canRead()) {
+                    throw new IOException("Cannot read file: " + filePath);
+                }
+                long length = f.length();
+                chunkLength = (int) length;
+                if (chunkLength != length)
+                    throw new IOException("Too big file size: " + length);
+
+                if (chunkLength == 0) {
+                    return "";
+                }
+
+                is = new FileInputStream(f);
+            }
+
+            Reader r;
+            if (charCoding == null) {
+                r = new InputStreamReader(is);
+            } else {
+                r = new InputStreamReader(is, charCoding);
+            }
+            return readReader(r, chunkLength);
+
+        } finally {
+            if (is != null)
+                is.close();
+        }
+    }
+
+    private static String readReader(Reader reader, int initialBufferSize)
+            throws IOException {
+        char[] buffer = new char[initialBufferSize];
+        int offset = 0;
+        for (;;) {
+            int n = reader.read(buffer, offset, buffer.length - offset);
+            if (n < 0) {
+                break;
+            }
+            offset += n;
+            if (offset == buffer.length) {
+                char[] tmp = new char[buffer.length * 2];
+                System.arraycopy(buffer, 0, tmp, 0, offset);
+                buffer = tmp;
+            }
+        }
+        return new String(buffer, 0, offset);
+    }
+
+    private static String getCharCodingFromType(String type)
+    {
+        int i = type.indexOf(';');
+        if (i >= 0) {
+            int end = type.length();
+            ++i;
+            while (i != end && type.charAt(i) <= ' ') {
+                ++i;
+            }
+            String charset = "charset";
+            if (charset.regionMatches(true, 0, type, i, charset.length()))
+            {
+                i += charset.length();
+                while (i != end && type.charAt(i) <= ' ') {
+                    ++i;
+                }
+                if (i != end && type.charAt(i) == '=') {
+                    ++i;
+                    while (i != end && type.charAt(i) <= ' ') {
+                        ++i;
+                    }
+                    if (i != end) {
+                        // i is at the start of non-empty
+                        // charCoding spec
+                        while (type.charAt(end -1) <= ' ') {
+                            --end;
+                        }
+                        return type.substring(i, end);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 };
